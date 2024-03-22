028b08d07b6eea38cdac37cbf0f9f9faaa8e08a4
Jeffrey <jeffreysoong888@gmail.com>
2024-03-22 09:18:08 +0700

    feat: added database code for initializing

---
+++ DatabaseExecutionCode.sql
@@ -0,0 +1,56 @@
+--Everything here is done in Mariadb, just remember to give the permissions
+--like
+CREATE DATABASE Chess;
+
+GRANT ALL PRIVILEGES ON Chess.* TO 'ssc'@'%';
+FLUSH PRIVILEGES;
+
+create table User
+(
+    ID       int auto_increment,
+    Username varchar(255)     not null,
+    Ratings  int default 1000 not null,
+    constraint User_pk_3
+        primary key (ID),
+    constraint User_pk
+        unique (ID),
+    constraint User_pk_2
+        unique (Username)
+);
+
+create table Game
+(
+    GameID  int auto_increment,
+    WhiteID int not null,
+    BlackID int not null,
+    constraint Game_pk_2
+        primary key (GameID),
+    constraint Game_pk
+        unique (GameID)
+);
+
+create table Moves
+(
+    MoveID int auto_increment,
+    GameID int not null,
+    constraint Moves_pk
+        primary key (MoveID),
+    constraint Moves_pk_2
+        unique (MoveID)
+);
+
+--Used to find all ID
+SELECT *
+FROM User;
+
+--Choose Game from the Player's UserID
+SELECT *
+FROM Game
+WHERE WhiteID = 'UserID' or BlackID = 'UserID';
+
+--Select the moves for the player
+SELECT *
+FROM Moves
+WHERE GameID = (SELECT GameID
+               From Game
+               WHERE WhiteID = 'UserID' or BlackID = 'UserID');
