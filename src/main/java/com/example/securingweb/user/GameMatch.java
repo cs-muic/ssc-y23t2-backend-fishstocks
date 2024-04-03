b4e30d1514a9f20ec081cfe9470e4f802dd2c881
chadakeng <chada.keng@gmail.com>
2024-04-03 15:13:51 +0700

    Added all args constructor to GameMatch class. Added updates to pom.xml

--- src/main/java/com/example/securingweb/user/GameMatch.java
+++ src/main/java/com/example/securingweb/user/GameMatch.java
@@ -2,13 +2,11 @@ package com.example.securingweb.user;
 
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @Builder
 @Data
 @Entity
