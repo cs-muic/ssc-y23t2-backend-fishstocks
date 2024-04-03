e1f01bf44923d7c5d50a1f3bcec2e31516828b87
Attaphat Chana <attaphart@gmail.com>
2024-04-03 18:22:52 +0700

    inject Repository annotation

--- src/main/java/com/example/securingweb/repo/GameMatchRepository.java
+++ src/main/java/com/example/securingweb/repo/GameMatchRepository.java
@@ -2,7 +2,10 @@ package com.example.securingweb.repo;
 
 import com.example.securingweb.user.GameMatch;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface GameMatchRepository extends JpaRepository<GameMatch, Long> {
     GameMatch findByPlayer1(String player1);
     GameMatch findByPlayer2(String player2);
